97cdcc0e837ec786675c7b3360db4a901f74b68b
roryn <rorynicolls@gmail.com>
2024-02-10 17:17:21 -0800

    Passed validation

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '65e2d1013a5d979c2c8a290d990857b9f9066ef7'
+AESD_ASSIGNMENTS_VERSION = 'e3a009511da152a2ad87dcf4331fa23ffa616e2a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,9 +23,15 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer.c $(TARGET_DIR)/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer.o $(TARGET_DIR)/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin/
+    $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin/
+	$(INSTALL) -m 0755 $(@D)/libc.so.6 $(TARGET_DIR)/lib64/
+	$(INSTALL) -m 0755 $(@D)/libresolv.so.2 $(TARGET_DIR)/lib64/
+	$(INSTALL) -m 0755 $(@D)/libm.so.6 $(TARGET_DIR)/lib64/
+	$(INSTALL) -m 0755 $(@D)/ld-linux-aarch64.so.1 $(TARGET_DIR)/lib/
 endef
 
 $(eval $(generic-package))
